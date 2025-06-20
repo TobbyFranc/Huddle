e24c9d9e740189fdc5bba17cc0740ec8cefbac0e
Tobi Francis Ogounz <77078820+TobbyFranc@users.noreply.github.com>
2025-06-20 05:20:29 -0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Huddle/', // Set the base path for GitHub Pages
   plugins: [
     tailwindcss(),
     react()],
